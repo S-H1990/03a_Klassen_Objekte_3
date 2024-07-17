7dd590a364ee180890b703be5d9cd0212c1149e9
Sara <Sarahakimi206@yahoo.com>
2024-07-17 12:17:46 +0200

    04/Cat-Tomcat refactoring

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,18 +5,17 @@ public class Cat {
   private String name;
   private String furColor;
   private int age; 
-  private boolean isFemale;
+
 
   private int counter;
 
-  public Cat(String name, String furColor, int age, boolean isFemale) {
+  
+  public Cat(String name, String furColor, int age) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
-    this.isFemale = isFemale;
   }
 
-  
 
 public String getStringAttributes(String op) {
   switch (op) {
@@ -31,12 +30,9 @@ public String getStringAttributes(String op) {
 
 public String getAge() {
 
-    if (!isFemale) {
-      return String.valueOf(age);
-    } else {
-       return checkCompliance();
-    }
     
+       return checkCompliance();
+  
 }
 
 private String checkCompliance(){
